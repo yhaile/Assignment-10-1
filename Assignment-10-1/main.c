7fe5863c8034ef2b8cfc2107e46d42aaeef30a8e
Yonas B Haile <yhaile@md3612-38177-14.matc.madison.login>
2013-09-16 13:26:30 -0500

    Initial Commit

---
+++ Assignment-10-1/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Assignment-10-1
+//
+//  Created by Yonas B Haile on 9/16/13.
+//  Copyright (c) 2013 Yonas B Haile. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
